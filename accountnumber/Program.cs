d2e7f6dafb51861263c06e377a5051c69ee9b2c9
akash4934 <patelakash517@gmail.com>
2016-12-20 09:45:52 -0500

    first Commit

---
+++ accountnumber/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace accountnumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            long acno;
+            Console.WriteLine("Please Enter Account Number : ");
+            acno = long.Parse(Console.ReadLine());
+            if( )
+            {
+                Console.WriteLine("it is a valid account number");
+                //Console.ReadKey();
+                long temp = acno % 10000;
+                Console.WriteLine(temp);
+            }
+            else
+            {
+                Console.WriteLine("Account Number Must be 10 to 15 Digits");
+                //Console.WriteLine("Please Re-Enter Account Number : ");
+                //acno = int.Parse(Console.ReadLine());
+                //Console.ReadKey();
+            }
+
+            
+            Console.ReadKey();
+
+        }
+    }
+}
